b8308baf8cefc9f49e6962037ce71a349fe8da96
vong <55168443+dangdinhvong@users.noreply.github.com>
2019-11-07 17:09:32 +0700

    done

---
+++ practice/Vong/chisocannang/chisocannang.js
@@ -0,0 +1,16 @@
+function calBMI() {
+    let weight = parseFloat(prompt('nhập vào cân nặng - kg'));
+    let height = parseFloat(prompt('nhập vào chiều cao - m'));
+    let BMI = weight / Math.pow(height,2);
+    let result;
+    if (BMI < 18.5) {
+        result = 'underweight';
+    } else if (BMI < 25 && BMI >= 18.5) {
+        result = 'normal';
+    } else if (BMI < 30 && BMI >= 25) {
+        result = 'overweight';
+    } else {
+        result = 'obese';
+    }
+    alert(result);
+}
