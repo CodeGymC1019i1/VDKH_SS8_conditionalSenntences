91917071f454da3170b5635b803a9b72c83e7f3a
leetrungkien <57204665+leetrungkien@users.noreply.github.com>
2019-11-08 08:17:43 +0700

    done

---
+++ exercise/Kien/tinhsongaytrongthang/numberDayOfMonth.js
@@ -0,0 +1,27 @@
+function countDayOfMonth() {
+    let month = parseInt(document.getElementById('month').value);
+    // let days = 0;
+    switch (month) {
+        case 1:
+        case 3:
+        case 5:
+        case 7:
+        case 8:
+        case 10:
+        case 12:
+            document.getElementById('result').value = 'tháng ' + month + ' có 31 ngày';
+            break;
+        case 2:
+            document.getElementById('result').value = 'tháng ' + month + ' có 28 hoặc 29 ngày';
+            break;
+        case 4:
+        case 6:
+        case 9:
+        case 11:
+            document.getElementById('result').value = 'tháng ' + month + '  có 30 ngày';
+            break;
+        default:
+            document.getElementById('result').value = month + 'không phải 1 tháng trong năm';
+            break;
+    }
+}
