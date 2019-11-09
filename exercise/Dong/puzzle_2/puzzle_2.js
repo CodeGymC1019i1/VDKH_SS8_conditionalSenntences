6638c219a1d61ef69642bb2dc930b73b9310215c
dong94vk <quangdong.071094@gmail.com>
2019-11-09 20:44:25 +0700

    bai tap *

--- exercise/Dong/puzzle_2/puzzle_2.js
+++ exercise/Dong/puzzle_2/puzzle_2.js
@@ -1,26 +1,3 @@
-
-function changeImg(image,src) {
-    //let img_1 = document.getElementById('img_1');
-    for(let i=1;i<=4;i++){
-        if (image.src.match(''+src+i+'_1.png')) {
-            image.src = ''+src+i+'_1.png';
-            alert
-        }
-    }
-    // if (image.src.match(src+'1'+'_1.png')) {
-    //     image.src = src+'1'+'_1.png';
-    // } else if (image.src.match('images/girl2_1.png')) {
-    //     image.src = 'images/girl3_1.png';
-    // } else if (image.src.match('images/girl3_1.png')) {
-    //     image.src = 'images/girl4_1.png';
-    // }else {
-    //     image.src = 'images/girl1_1.png';
-    // }
-    compareSrc();
-}
-
-
-
 function changeImg1() {
     let img_1 = document.getElementById('img_1');
     if (img_1.src.match('images/girl1_1.png')) {
@@ -29,7 +6,7 @@ function changeImg1() {
         img_1.src = 'images/girl3_1.png';
     } else if (img_1.src.match('images/girl3_1.png')) {
         img_1.src = 'images/girl4_1.png';
-    }else {
+    } else {
         img_1.src = 'images/girl1_1.png';
     }
     compareSrc();
@@ -43,11 +20,12 @@ function changeImg2() {
         img_2.src = 'images/girl2_2.png';
     } else if (img_2.src.match('images/girl2_2.png')) {
         img_2.src = 'images/girl1_2.png';
-    }else {
+    } else {
         img_2.src = 'images/girl4_2.png';
     }
     compareSrc();
 }
+
 function changeImg3() {
     let img_3 = document.getElementById('img_3');
     if (img_3.src.match('images/girl2_3.png')) {
@@ -56,11 +34,12 @@ function changeImg3() {
         img_3.src = 'images/girl4_3.png';
     } else if (img_3.src.match('images/girl4_3.png')) {
         img_3.src = 'images/girl1_3.png';
-    }else {
+    } else {
         img_3.src = 'images/girl2_3.png';
     }
     compareSrc();
 }
+
 function changeImg4() {
     let img_4 = document.getElementById('img_4');
     if (img_4.src.match('images/girl3_4.png')) {
@@ -69,21 +48,22 @@ function changeImg4() {
         img_4.src = 'images/girl2_4.png';
     } else if (img_4.src.match('images/girl2_4.png')) {
         img_4.src = 'images/girl1_4.png';
-    }else {
+    } else {
         img_4.src = 'images/girl3_4.png';
     }
     compareSrc();
 }
+
 function compareSrc() {
     let img_1 = document.getElementById('img_1');
     let img_2 = document.getElementById('img_2');
     let img_3 = document.getElementById('img_3');
     let img_4 = document.getElementById('img_4');
-    let a = img_1.src.substr(0,img_1.src.length-6);
-    let b = img_2.src.substr(0,img_2.src.length-6);
-    let c = img_3.src.substr(0,img_3.src.length-6);
-    let d = img_4.src.substr(0,img_4.src.length-6);
-    if(a===b && a===c && a===d){
+    let a = img_1.src.substr(0, img_1.src.length - 6);
+    let b = img_2.src.substr(0, img_2.src.length - 6);
+    let c = img_3.src.substr(0, img_3.src.length - 6);
+    let d = img_4.src.substr(0, img_4.src.length - 6);
+    if (a === b && a === c && a === d) {
         alert('done');
     }
 }
